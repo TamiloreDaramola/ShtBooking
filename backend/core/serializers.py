1ef11b3b41e9ae8d8f3d4025118cd6fb91ad918b
Daramola Tamilore <oreayodaramola@gmail.com>
2025-09-11 13:08:55 +0100

    Commit 4 i think

---
+++ backend/core/serializers.py
@@ -0,0 +1,17 @@
+# shortlet/backend/core/serializers.py
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'is_host', 'is_guest']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
