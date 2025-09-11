332ffe7fb72d51355cb9e4b06d38bb20d336be3b
Daramola Tamilore <oreayodaramola@gmail.com>
2025-09-11 13:11:11 +0100

    Commit 5

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
 
-# Create your views here.
+# Create your views here.# shortlet/backend/core/views.py
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserSerializer
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(TokenObtainPairView):
+    pass
